89402edc8ddcb9638ac67f544405bba4ca933b3e
Ansh-Rajput <anshravikrish425317@gmail.com>
2023-08-23 23:56:24 +0530

    Basic Ui of spotify compleated.

---
+++ components/SideBar.jsx
@@ -0,0 +1,53 @@
+'use client'
+
+import React, { useMemo } from 'react'
+import {usePathname} from 'next/navigation'
+import {HiHome} from 'react-icons/hi'
+import {BiSearch} from 'react-icons/bi'
+import Box from './Box'
+import SideBarItem from './SideBarItem'
+import Library from './Library'
+
+const SideBar = ({children}) => {
+    const pathName = usePathname();
+    const routes = useMemo(()=>[
+        {
+            icon:HiHome,
+            label:'Home',
+            active:pathName !== '/search',
+            href:'/',
+        },
+        {
+            icon:BiSearch,
+            label:'Search',
+            active:pathName === '/search',
+            href:'/search',
+        },
+    ],[])
+  return (
+    <div className='flex h-full'>
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box>
+            <div className="flex flex-col gap-y-4 px-5 py-4">
+                {
+                    routes.map((item)=>{
+                       return <SideBarItem
+                            key={item.label}
+                            {...item}
+                        />
+                    })
+                }
+            </div>
+        </Box>
+        <Box className='overflow-y-auto h-full'>
+          <Library/>
+        </Box>
+      </div>
+      <main className='h-full flex-1 overflow-y-auto py-2'>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default SideBar
